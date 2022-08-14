9efb274815ff7c57a1318132675a45d11e2ee36c
Justin Chung <jdchung@scu.edu>
2022-08-14 02:42:05 -0700

    Added comments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,16 @@
 const sumAll = function(first, second) {
+    // Parameters cannot be negative
     if(first < 0 || second < 0)
         return 'ERROR';
     
+    // Parameters must be integers
     if(!Number.isInteger(first) || !Number.isInteger(second))
         return 'ERROR';
 
     let sum = 0;
     let temp;
 
+    // Swaps first and second parameter if first is larger than the second
     if(first > second)
     {
         temp = first;
@@ -15,6 +18,7 @@ const sumAll = function(first, second) {
         second = temp;
     }
 
+    // Adds numbers between two different numbers
     for(let i = first; i <= second; i++)
     {
         sum +=i;
