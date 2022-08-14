56b3832aabb16177a648fb7c2ada8d3b3e227c96
Justin Chung <jdchung@scu.edu>
2022-08-14 02:48:06 -0700

    Completed leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // returns true if qualifies for a leap year, false otherwise
+    if((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+        return true;
+    return false;
+
+    /* Faster method 
+    return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0);
+    */
 
 };
 
