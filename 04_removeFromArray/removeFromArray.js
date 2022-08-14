301b2720a1eaf6de369b0d1e50f237b2d4144526
Justin Chung <jdchung@scu.edu>
2022-08-13 23:06:05 -0700

    Completed removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,19 @@
-const removeFromArray = function(array, arg) {
-    const index = array.indexOf(arg);
-    array.splice(index, 1);
-    return array;
+const removeFromArray = function(...arg) {
+    // Takes first element from rest parameter, which is the original array
+    const array = arg[0];
+
+    // Create empty array
+    const newArray = [];
+
+    // For each item in array, if the item is not included in the arguments, push item into new array    
+    array.forEach((item) => {
+        if(!arg.includes(item)){
+            newArray.push(item);
+        }
+    });
+
+    // Return new array
+    return newArray;
 };
 
 // Do not edit below this line
