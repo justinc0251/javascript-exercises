b60f7a1f06896d08756dcc555104baf2fb3cdd33
Justin Chung <jdchung@scu.edu>
2022-08-13 22:45:45 -0700

    Remove one item from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, arg) {
+    const index = array.indexOf(arg);
+    array.splice(index, 1);
+    return array;
 };
 
 // Do not edit below this line
