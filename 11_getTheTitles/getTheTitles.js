fdbf94624e37fc0c2abef27777b4b80e50df77a0
Justin Chung <jdchung@scu.edu>
2022-09-06 13:09:11 -0700

    Completed getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (array) {
+    let answer = [];
+    array.forEach(book => answer.push(book.title));
+    return answer;
+  // return array.map((book) => book.title);
 };
 
 // Do not edit below this line
