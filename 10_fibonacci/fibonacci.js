7d3f7b9f56d0895b21c0b07af8351bd00243fa35
Justin Chung <jdchung@scu.edu>
2022-09-06 00:36:27 -0700

    Finished fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+// 1 + 1 + 2 + 3 + 5 + 8
 
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS";
+  }
+
+  if (num == 0) {
+    return 0;
+  }
+
+  let answer = 0;
+  let temp;
+
+  num1 = 0;
+  num2 = 1;
+
+  for (i = 1; i < num; i++) {
+    temp = num2;
+    num2 = num1 + num2;
+    num1 = temp;
+  }
+  return num2;
 };
 
 // Do not edit below this line
