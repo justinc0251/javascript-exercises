5776c4bb2e7600bc45654ba381fd38f75e1a57bb
Justin Chung <jdchung@scu.edu>
2022-09-05 17:49:07 -0700

    completed Calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+
+  // return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
+const multiply = function (array) {
+  let product = array[0];
+  for (let i = 1; i < array.length; i++) {
+    product *= array[i];
+  }
+  return product;
 
+  // return array.length ? array.reduce((accumulator, nextItem) => accumulator * nextItem) : 0;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number == 0) return 1;
+  let answer = 1;
+  for (let i = number; i > 0; i--) {
+    answer *= i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
@@ -29,5 +46,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
