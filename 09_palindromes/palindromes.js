f5f9ef7537386aa91bcc307820e26d7bebf935f3
Justin Chung <jdchung@scu.edu>
2022-09-06 00:23:13 -0700

    Finished palindromes exercse

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  let lowerWord = word.toLowerCase();
+  let array = [];
+  let tempArray = lowerWord.split("");
+
+  for (let i = 0; i < tempArray.length; i++) {
+    if (tempArray[i] >= "a" && tempArray[i] <= "z") {
+      array.push(tempArray[i]);
+    }
+  }
+  let string = array.join("");
+
+  return string.split("").reverse().join("") == string;
 
 };
 
+/**
+ * const palindromes = function(string) {
+  processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+  return (
+    processedString
+      .split("")
+      .reverse()
+      .join("") == processedString
+  );
+};
+ */
 // Do not edit below this line
 module.exports = palindromes;
